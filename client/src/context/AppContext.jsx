ba35f4f5408004b94b4d3a2225f8e847a80ade50
zarif50 <zarifmahmud250@gmail.com>
2026-08-12 14:56:07 +0600

    user authentication done

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,11 +1,12 @@
-import { createContext, useContext, useState,useEffect } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import api from '../api/api.js'
-
+import toast from "react-hot-toast"
+import{useNavigate} from "react-router-dom"
 const AppContext = createContext(undefined);
 
 export function AppContextProvider({ children }) {
 
-
+      const navigate = useNavigate()
     //auth
     const [user, setUser] = useState(null);
     const [loadingUser, setLoadingUser] = useState(true);
@@ -14,7 +15,7 @@ export function AppContextProvider({ children }) {
 
     const checkSession = async () => {
         try {
-            const {data} = await api.get("/api/auth/me");
+            const { data } = await api.get("/api/auth/me");
             setUser(data.user);
         } catch (error) {
             setUser(null);
@@ -28,10 +29,45 @@ export function AppContextProvider({ children }) {
         checkSession()
     }, [checkSession]);
 
+
+    const login = async (email, password) => {
+        try {
+
+            const { data } = await api.post("/api/auth/login", { email, password });
+            setUser(data.user)
+            toast.success("welcome back!")
+            navigate("/")
+        } catch (err) {
+            console.error("login Failed:", err);
+            const errMsg = err?.response?.data?.error ||  "Inavlid email or password";
+            toast.error(errMsg);
+            throw new Error(errMsg);
+        }
+    }
+
+    const register = async (name, email, password) => {
+        try {
+
+            const { data } = await api.post("/api/auth/register", {name, email, password });
+            setUser(data.user)
+            toast.success("Account Created Suceesfully!")
+            navigate("/")
+        } catch (err) {
+            console.error("Registration Failed:", err);
+            const errMsg = err?.response?.data?.error ||  "Registration failed";
+            toast.error(errMsg);
+            throw new Error(errMsg);
+        }
+    }
+
+
+
     return (
         <AppContext.Provider value={{
             user,
-            loadingUser
+            loadingUser,
+            login,
+            register
         }}>
             {children}
         </AppContext.Provider>
--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import LoginLeft from '../components/LoginLeft.jsx'
 import { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link ,useNavigate} from 'react-router-dom';
+import { EyeIcon, EyeOffIcon,Loader2Icon } from 'lucide-react';
+import { useAppContext } from '../context/AppContext.jsx';
 
 
 const AuthPage = ({ mode }) => {
 
+  const navigate = useNavigate()
+  const{login,register}= useAppContext()
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
   const [name, setName] = useState("");
@@ -15,6 +19,25 @@ const AuthPage = ({ mode }) => {
 
 
   const isLogin = mode === "login";
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    setError("")
+    setLoading(true)
+
+    try{
+            if(mode === "login"){
+              await login(email,password)
+            }else{
+              await register(name,email,password)
+            }
+               navigate("/")
+    } catch(err){
+          setError(err.message || (mode==="login"? "Invalid email or Password":"Registration failed"))
+    }finally{
+      setLoading(false)
+    }
+  }
   return (
     <div className="min-h-screen bg-white flex text-zinc-900 font-sans">
 
@@ -25,16 +48,16 @@ const AuthPage = ({ mode }) => {
         <div className="w-full max-w-sm">
           <div className="mb-10">
             <h1 className="text-3xl font-medium tracking-tight text-zinc-900 mb-1.5 font-sans">{isLogin ? "Sign In " : "Create Account"}</h1>
-            <p className="text-zinc-400 text-sm">
+            <p className="text-zinc-800 text-sm">
               {isLogin ? "Enter your Credentials to access your website builder" : "Get started by entering your details below"}
             </p>
           </div>
           {error && <div className='mb-6 p-3 border border-red-200 bg-red-50 text-red-700 text-xs rounded'>{error}</div>}
 
-          <form className='space-y-6'>
+          <form className='space-y-6' onSubmit={handleSubmit}>
             {!isLogin && (
               <div>
-                <label className='block text-[11px] font-semibold text-zinc-400 uppercase tracking-widest mb-2'>
+                <label className='block text-[11px] font-semibold text-zinc-700 uppercase tracking-widest mb-2'>
                   Full Name
                 </label>
                 <input type="text" value={name} onChange={(e) => setName(e.target.value)} required className='w-full pl-2 py-2 border-b border-zinc-200 focus:outline-none
@@ -45,7 +68,7 @@ const AuthPage = ({ mode }) => {
 
 
             <div>
-              <label className='block text-[11px] font-semibold text-zinc-400 uppercase tracking-widest mb-2'>
+              <label className='block text-[11px] font-semibold text-zinc-700 uppercase tracking-widest mb-2'>
                 Email Address
               </label>
               <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required className='w-full pl-2 py-2 border-b border-zinc-200 focus:outline-none
@@ -53,30 +76,56 @@ const AuthPage = ({ mode }) => {
             </div>
 
             <div>
-              <label className='block text-[11px] font-semibold text-zinc-400 uppercase tracking-widest mb-2'>
+              <label className="block text-[11px] font-semibold text-zinc-700 uppercase tracking-widest mb-2">
                 Password
               </label>
 
-               
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  placeholder="**********"
+                  className="w-full pl-2 py-2 border-b border-zinc-200 bg-transparent text-sm text-zinc-900 placeholder-zinc-300 focus:outline-none focus:border-zinc-950 transition-colors"
+                />
 
-
-              <input type={showPassword ? "text" : "password"} value={password} onChange={(e) => setEmail(e.target.value)} required className='w-full pl-2 py-2 border-b border-zinc-200 focus:outline-none
-                       focus:border-zinc-950 text-sm text-zinc-900 bg-transparent placeholder-zinc-300 transition-colors' placeholder='**********' />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-2 top-1/2 -translate-y-1/2 text-zinc-400 hover:text-zinc-700 flex items-center justify-center transition-colors"
+                >
+                  {showPassword ? (
+                    <EyeOffIcon size={18} />
+                  ) : (
+                    <EyeIcon size={18} />
+                  )}
+                </button>
+              </div>
             </div>
+
+              <button type='submit' disabled={loading}
+              className='w-full py-2.5 bg-linear-to-br from-teal-900 to-cyan-950 text-white font-semibold hover:scale-102 disabled:opacity-40 flex items-center
+              justify-center cursor-pointer mt-2 rounded-lg transition-all'>
+                  {loading && <Loader2Icon className='animate-spin h-3.5 w-3.5 mr-2'/>}
+                  {isLogin ? "Sign in" :"Sign up"}
+              </button>
           </form>
 
-          <p>
+          <p className='text-sm text-zinc-400 mt-8 pt-6 border-t border-zinc-100'>
             {isLogin ? (
               <>
                 New to BuilderAI?{" "}
-                <Link to="/register" className="text-zinc-900 font-medium hover:underline">
+                <Link to="/register" className="text-zinc-900 font-medium hover:underline"
+                className="text-teal-900 font-medium hover:underline">
                   Create an account
                 </Link>
               </>
             ) : (
               <>
                 Already have an account?{" "}
-                <Link to="/login" className="text-zinc-900 font-medium hover:underline">
+                <Link to="/login" className="text-zinc-900 font-medium hover:underline"
+                className="text-teal-900 font-medium hover:underline">
                   Sign in here
                 </Link>
               </>
