9a986cb44f62599d0c0ab8642a85ee0b934db2b4
zarif50 <zarifmahmud250@gmail.com>
2026-08-13 11:51:00 +0600

    Home page done

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,15 +1,26 @@
-import { createContext, useContext, useState, useEffect } from 'react'
+import { createContext, useContext, useState, useEffect,useCallback } from 'react'
 import api from '../api/api.js'
 import toast from "react-hot-toast"
-import{useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 const AppContext = createContext(undefined);
 
 export function AppContextProvider({ children }) {
 
-      const navigate = useNavigate()
+    const navigate = useNavigate()
     //auth
     const [user, setUser] = useState(null);
     const [loadingUser, setLoadingUser] = useState(true);
+
+    //states
+    const [projects, setProjects] = useState([]);
+    const [loadingProjects, setLoadingProjects] = useState([true]);
+    const [activeProject, setActiveProject] = useState([null]);
+    const [loadingActiveProject, setLoadingActiveProject] = useState(true);
+    const [chatLoading, setChatLoading] = useState(false);
+    const [generatingProject, setGeneratingProject] = useState(false);
+    const [activeFile, setActiveFile] = useState("/App.js");
+    const [showCode, setShowCode] = useState(false);
+
 
     //auth action
 
@@ -39,22 +50,128 @@ export function AppContextProvider({ children }) {
             navigate("/")
         } catch (err) {
             console.error("login Failed:", err);
-            const errMsg = err?.response?.data?.error ||  "Inavlid email or password";
+            const errMsg = err?.response?.data?.error || "Inavlid email or password";
             toast.error(errMsg);
             throw new Error(errMsg);
         }
     }
+    const logout = async () => {
+        try {
+            await api.post("/api/auth/logout")
+            setUser(null)
+            setProjects([])
+            setActiveProject(null)
+            toast.success("Logged out Successfully")
+            navigate("/login")
+        } catch (error) {
+            console.error("Logout Failed:", err)
+            toast.error("Logout Failed")
+        }
+    }
+
+    //projects action
+    const loadProjects = async () => {
+        if (!user) return;
+        try {
+            const { data } = await api.get("/api/projects")
+            setProjects(data)
+        } catch (err) {
+            console.error("Failed to list projects", err);
+            toast.error("Failed to load Projects list");
+
+        } finally {
+            setLoadingProjects(false);
+        }
+    }
+
+    const loadProject = async (id, silent = false) => {
+        if (!user) return;
+        if (!silent) setLoadingActiveProject(true)
+        try {
+            const { data } = await api.get(`/api/projects/${id}`)
+            setActiveProject(data);
+            const files = Object.keys(data.files);
+            if (files.length > 0) {
+                setActiveFile((prev) => {
+                    if (files.includes(prev)) return prev;
+                    if (files.includes("/App.js")) return "App/js";
+                    return files[0]
+                })
+            }
+        } catch (err) {
+            console.error("Failed to load Projects:", err);
+            if (!silent) {
+                toast.error("Failed to load Projects details");
+                navigate("/");
+            }
+        } finally {
+            if (!silent) setLoadingActiveProject(false)
+        }
+    }
+    ///automatically poll active project status(pend or generating)
+    useEffect(() => {
+        if (!activeProject?._id || user) return;
+
+        const isOngoing = activeProject.status === "generating" || activeProject.status === "pending" || activeProject.status ===
+            "revising";
+
+        if (isOngoing) {
+            setChatLoading(true);
+            const interval = setInterval(() => {
+                loadProject(activeProject._id, true)
+            }, 2000);
+            return () => clearInterval(interval)
+        } else {
+            setChatLoading(false);
+        }
+
+    }, [activeProject?._id, activeProject?.status, loadProject, user])
+
+    const handleGenerate = useCallback(
+        async (prompt) => {
+            if (!user) return;
+            setGeneratingProject(true);
+            try {
+                const { data } = await api.post("/api/projects", { prompt });
+                toast.success("Ai Agent is planning structure...")
+                navigate(`/buider/${data.id}`);
+            } catch (err) {
+                console.error("Failed to generate projects:", err);
+                toast.error(err?.response?.data?.error || " Failed to generate project");
+            } finally {
+                setGeneratingProject(false);
+            }
+
+        }, [navigate, user]
+    )
+
+    const handleDelete = useCallback(
+        async (id) => {
+            if (!user) return;
+            try {
+                await api.delete(`/api/projects/${id}`);
+                setProjects((prev) => prev.filter((p) => p._id !== id))
+                toast.success("Project deleted successfully");
+            }
+            catch (err) {
+                console.error("Failed to generate projects:", err);
+                toast.error("failed to delete project");
+            }
+
+        }, [user]
+    )
+
 
     const register = async (name, email, password) => {
         try {
 
-            const { data } = await api.post("/api/auth/register", {name, email, password });
+            const { data } = await api.post("/api/auth/register", { name, email, password });
             setUser(data.user)
             toast.success("Account Created Suceesfully!")
             navigate("/")
         } catch (err) {
             console.error("Registration Failed:", err);
-            const errMsg = err?.response?.data?.error ||  "Registration failed";
+            const errMsg = err?.response?.data?.error || "Registration failed";
             toast.error(errMsg);
             throw new Error(errMsg);
         }
@@ -67,7 +184,22 @@ export function AppContextProvider({ children }) {
             user,
             loadingUser,
             login,
-            register
+            register,
+            projects,
+            loadProjects,
+            activeProject,
+            loadingActiveProject,
+            chatLoading,
+            generatingProject,
+            activeFile,
+            showCode,
+            setActiveFile,
+            setShowCode,
+            loadProjects,
+            loadProject,
+            handleGenerate,
+            handleDelete
+
         }}>
             {children}
         </AppContext.Provider>
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useAppContext } from "../context/AppContext.jsx";
 import PromptInput from "../components/PromptInput.jsx";
+import { homeTags } from "../assets/assets.js";
 
 const Homepage = () => {
-  const { user } = useAppContext();
+  const { user, projects,loadingProjects,generatingProject,loadProjects,handleGenerate,handleDelete,logout } = useAppContext();
 
   return (
     <div className="h-screen overflow-y-scroll text-white font-sans bg-[url('/bg-img.png')] bg-cover bg-center bg-no-repeat">
@@ -27,11 +28,11 @@ const Homepage = () => {
         <div className="flex items-center gap-4 text-sm font-medium text-zinc-300">
           <span>{user?.name}</span>
 
-          <button
+          <button onClick={logout}
             className="py-1.5 px-3 border border-white/20 text-white
             hover:bg-white/10 text-xs rounded-md cursor-pointer"
           >
-            Logout
+           Sign out
           </button>
         </div>
       </nav>
@@ -81,14 +82,28 @@ const Homepage = () => {
           {/* Prompt Input */}
           <div className="w-full mt-6">
             <PromptInput
-              onSubmit={() => {}}
-              loading={false}
+              onSubmit={handleGenerate}
+              loading={generatingProject}
               placeholder="Create a Portfolio Website..."
               variant="glass"
               autoFocus
             />
           </div>
+              {/*scrolling marque tags*/}
+              <div className="masked-marquee w-full mt-4 max-w-2xl overflow-hidden py-1">
+                <div className="animate-marquee gap-3">
+                    {homeTags.map((tag,i)=>(
+                      <button key={i} onClick={()=>handleGenerate(tag)}
+                      disabled={generatingProject}
+                      className="px-4 py-1.5 border rounded-full text-sm text-white bg-white/10border-white/25
+                      hover:bg-white/20 transition cursor-pointer shrink-0 font-medium">
+                        {tag}
 
+                      </button>
+                    ))}
+                </div>
+
+              </div>
         </div>
       </div>
     </div>
